fee9f51bc3768e84f00c077951ce61bd5c6aae10
optimisme <optimisme@gmail.com>
2023-11-22 18:10:13 +0100

    .

--- gh-pages/example/flutter_service_worker.js
+++ gh-pages/example/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "ff966ab969ba381b900e61629bfb9789",
-"index.html": "49e128d56c3ca2de19b986aa40e57aa0",
-"/": "49e128d56c3ca2de19b986aa40e57aa0",
-"main.dart.js": "5a89fc2d9e94f7e41bf1df36c092a8d7",
+"index.html": "24d358e3e3cad94197fa5cc1c5b4689d",
+"/": "24d358e3e3cad94197fa5cc1c5b4689d",
+"main.dart.js": "9f332eae5d682caaf9921829b074c1bb",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
