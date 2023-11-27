df471cd892ff87df97079b9afac9bd0cecabce73
optimisme <optimisme@gmail.com>
2023-11-27 08:15:32 +0100

    .

--- example/lib/layout_dialogs.dart
+++ example/lib/layout_dialogs.dart
@@ -10,6 +10,7 @@ class LayoutDialogs extends StatefulWidget {
 
 class _LayoutDialogsState extends State<LayoutDialogs> {
   // Used to tell the popover where to show up
+  final GlobalKey<CDKDialogPopoverState> _anchorPopoverSlider = GlobalKey();
   final GlobalKey<CDKDialogPopoverState> _anchorPopover0 = GlobalKey();
   final GlobalKey<CDKDialogPopoverState> _anchorPopover1 = GlobalKey();
   final GlobalKey<CDKDialogPopoverState> _anchorPopover2 = GlobalKey();
@@ -22,6 +23,7 @@ class _LayoutDialogsState extends State<LayoutDialogs> {
   final GlobalKey<CDKDialogPopoverState> _anchorArrowed1 = GlobalKey();
   final GlobalKey<CDKDialogPopoverState> _anchorArrowed2 = GlobalKey();
   final GlobalKey<CDKDialogPopoverState> _anchorArrowed3 = GlobalKey();
+  final ValueNotifier<double> _sliderValueNotifier = ValueNotifier(0.5);
 
   _showPopover(BuildContext context, GlobalKey anchorKey, CDKTheme theme,
       bool centered, bool animated, bool translucent) {
@@ -158,30 +160,60 @@ class _LayoutDialogsState extends State<LayoutDialogs> {
     );
   }
 
+  _showPopoverSlider(
+      BuildContext context, GlobalKey anchorKey, CDKTheme theme) {
+    final GlobalKey<CDKDialogPopoverArrowedState> key = GlobalKey();
+    CDKDialogsManager.showPopoverArrowed(
+      key: key,
+      context: context,
+      anchorKey: anchorKey,
+      isAnimated: true,
+      isTranslucent: false,
+      onHide: () {
+        // ignore: avoid_print
+        print("hide popover $key");
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ValueListenableBuilder<double>(
+            valueListenable: _sliderValueNotifier,
+            builder: (context, value, child) {
+              return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Slider:", style: TextStyle(fontSize: 16)),
+                    Text(value.toStringAsFixed(2),
+                        style: const TextStyle(fontSize: 12)),
+                    SizedBox(
+                      height: 20,
+                      width: 200,
+                      child: CDKPickerSlider(
+                        value: value,
+                        onChanged: (value) {
+                          _sliderValueNotifier.value = value;
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: () {
+                        key.currentState?.hide();
+                      },
+                      child: Text('Close popover',
+                          style: TextStyle(fontSize: 12, color: theme.accent)),
+                    ),
+                  ]);
+            }),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     CDKTheme theme = CDKThemeNotifier.of(context)!.changeNotifier;
 
     return ListView(children: [
       const SizedBox(height: 8),
-      Padding(
-          padding: const EdgeInsets.all(8),
-          child: RichText(
-            text: TextSpan(
-              style: TextStyle(fontSize: 14, color: theme.colorText),
-              children: const <TextSpan>[
-                TextSpan(
-                    text: '*Important! ',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(
-                    text:
-                        'The shadow of translucent dialogs is not drawn properly on the web.'),
-              ],
-            ),
-          )),
-      const SizedBox(height: 8),
-      const Padding(
-          padding: EdgeInsets.all(8), child: Text('CDKDialogPopover:')),
       Wrap(children: [
         Padding(
             padding: const EdgeInsets.all(8),
@@ -372,6 +404,27 @@ class _LayoutDialogsState extends State<LayoutDialogs> {
               },
               child: const Text('Translucent with animation'),
             )),
+      ]),
+      const SizedBox(height: 8),
+      Wrap(crossAxisAlignment: WrapCrossAlignment.center, children: [
+        Padding(
+            padding: const EdgeInsets.all(8),
+            child: CDKButton(
+              key: _anchorPopoverSlider,
+              style: CDKButtonStyle.normal,
+              isLarge: false,
+              onPressed: () {
+                _showPopoverSlider(context, _anchorPopoverSlider, theme);
+              },
+              child: const Text('Popover with slider'),
+            )),
+        ValueListenableBuilder<double>(
+          valueListenable: _sliderValueNotifier,
+          builder: (context, value, child) {
+            return Text('Slider value: ${value.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 12));
+          },
+        )
       ]),
       const SizedBox(height: 50),
     ]);
