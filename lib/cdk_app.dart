1f923dd6c4288632caa2c7f7dcfcc5b9378c1f35
optimisme <optimisme@gmail.com>
2023-12-01 16:29:46 +0100

    .

--- lib/cdk_app.dart
+++ lib/cdk_app.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_cupertino_desktop_kit/cdk_widgets.dart';
-import 'cdk_theme.dart';
-import 'cdk_theme_notifier.dart';
 
 // Copyright © 2023 Albert Palacios. All Rights Reserved.
 // Licensed under the BSD 3-clause license, see LICENSE file for details.
