355fef19a7d36ba73e0d185016365453a8d9e2d8
achivaq01 <achicavaquerizo.cf@iesesteveterradas.cat>
2023-12-05 16:00:11 +0100

    Documented cdk_button_color.dart, cdk_button_discsure.dart, cdk_picker_check_list.dart and cdk_picker_color.dart

--- lib/cdk_button_disclosure.dart
+++ lib/cdk_button_disclosure.dart
@@ -5,6 +5,29 @@ import 'cdk_theme_notifier.dart';
 // Copyright © 2023 Albert Palacios. All Rights Reserved.
 // Licensed under the BSD 3-clause license, see LICENSE file for details.
 
+/// A disclosure button widget for Flutter applications with animated rotation.
+///
+/// The [CDKButtonDisclosure] widget provides a disclosure button that can be toggled
+/// to reveal or hide additional content. It includes an animation that rotates the
+/// chevron icon when the button is toggled.
+///
+/// <img src="/flutter_cupertino_desktop_kit/gh-pages/doc-images/CDKButtonDisclosure_0.png" alt="CDKButtonDisclosure Example 1" style="max-width: 500px; width: 100%;">
+/// <img src="/flutter_cupertino_desktop_kit/gh-pages/doc-images/CDKButtonDisclosure_1.png" alt="CDKButtonDisclosure Example 2" style="max-width: 500px; width: 100%;">
+///
+/// ```dart
+/// CDKButtonDisclosure(
+///   value: true,
+///   onChanged: (newValue) {
+///     // Handle disclosure state change
+///   },
+///   size: 16.0,
+/// )
+/// ```
+///
+/// Parameters:
+/// * `value`: The current state of the disclosure button, whether it is open or closed.
+/// * `onChanged`: A callback function that is called when the disclosure state changes.
+/// * `size`: The size of the disclosure button. Defaults to `16.0`.
 class CDKButtonDisclosure extends StatefulWidget {
   final bool value;
   final ValueChanged<bool>? onChanged;
