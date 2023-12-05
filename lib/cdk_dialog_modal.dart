a9e875cb723f0b58c94d36016517c842acdd4353
PNaharro <96410598+PNaharro@users.noreply.github.com>
2023-12-05 16:31:02 +0100

    Update cdk_dialog_modal.dart

--- lib/cdk_dialog_modal.dart
+++ lib/cdk_dialog_modal.dart
@@ -9,6 +9,7 @@ import 'cdk_theme.dart';
 // Licensed under the BSD 3-clause license, see LICENSE file for details.
 
 class CDKDialogModal extends StatefulWidget {
+  // Constructor parameters for the CDKDialogModal widget
   final bool isAnimated;
   final bool isTranslucent;
   final Function? onHide;
@@ -26,8 +27,10 @@ class CDKDialogModal extends StatefulWidget {
   CDKDialogModalState createState() => CDKDialogModalState();
 }
 
+// State class for CDKDialogModal
 class CDKDialogModalState extends State<CDKDialogModal>
     with SingleTickerProviderStateMixin {
+  // Instance variables
   OverlayEntry? overlayEntry;
   final int _animationMillis = 200;
   AnimationController? animationController;
@@ -36,35 +39,37 @@ class CDKDialogModalState extends State<CDKDialogModal>
   double? height;
   bool isSizeDetermined = false;
   GlobalKey childKey = GlobalKey();
-  Offset position = const Offset(
-      -10000000, -10000000); // Out of view until size is determined
+  Offset position = const Offset(-10000000, -10000000); // Out of view until size is determined
   Path pathContour = Path();
   Path pathClip = Path();
 
   @override
   void initState() {
     super.initState();
-
+    // Initialization logic when the widget is inserted into the tree
     if (widget.isAnimated) {
+      // Setup animation controller if animation is enabled
       animationController = AnimationController(
         duration: Duration(milliseconds: _animationMillis),
         vsync: this,
       );
 
-      // Efecte de rebot
+      // Apply bounce effect to the animation
       scaleAnimation = CurvedAnimation(
         parent: animationController!,
         curve: Curves.easeOutBack,
       );
     }
 
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    // Execute code after the first frame is rendered
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
       if (mounted && childKey.currentContext != null) {
         final RenderBox childRenderBox =
             childKey.currentContext!.findRenderObject() as RenderBox;
         final childSize = childRenderBox.size;
 
         setState(() {
+          // Determine size and position of the dialog based on screen dimensions
           width = childSize.width;
           height = childSize.height;
           isSizeDetermined = true;
@@ -105,24 +110,29 @@ class CDKDialogModalState extends State<CDKDialogModal>
 
   @override
   void dispose() {
+    // Dispose of animation controller when the state is disposed
     animationController?.dispose();
     super.dispose();
   }
 
+  // Method to hide the dialog
   void hide() {
     widget.onHide?.call();
   }
 
   @override
   Widget build(BuildContext context) {
+    // Obtain theme information
     CDKTheme theme = CDKThemeNotifier.of(context)!.changeNotifier;
 
+    // Determine background color based on transparency
     Color backgroundColor = !widget.isTranslucent
         ? theme.background
         : theme.isLight
             ? theme.background.withOpacity(0.25)
             : theme.background.withOpacity(0.5);
 
+    // Create dialog contents widget
     Widget dialogContents = Container(
       key: childKey,
       decoration: BoxDecoration(
@@ -131,10 +141,12 @@ class CDKDialogModalState extends State<CDKDialogModal>
       child: widget.child,
     );
 
+    // Construct the final widget tree with decorations
     Widget dialogWithDecorations = !isSizeDetermined
         ? Container()
         : Stack(
             children: [
+              // Paint outer shadow
               CustomPaint(
                 painter: CDKDialogOuterShadowPainter(
                     pathContour: pathContour,
@@ -142,6 +154,7 @@ class CDKDialogModalState extends State<CDKDialogModal>
                     isLightTheme: theme.isLight),
                 child: Container(),
               ),
+              // Position the dialog contents with clipping and blur for translucent dialogs
               Positioned(
                   top: 8,
                   left: 8,
@@ -149,17 +162,18 @@ class CDKDialogModalState extends State<CDKDialogModal>
                       ? dialogContents
                       : ClipPath(
                           clipper: CDKPopoverClipper(
-                              pathClip), // Aplica el clip aquí
+                              pathClip), // Apply the clip here
                           child: BackdropFilter(
                             filter: ImageFilter.blur(
                                 sigmaX: 7.5,
-                                sigmaY: 7.5), // Efecte de difuminat
+                                sigmaY: 7.5), // Apply blur effect
                             child: dialogContents,
                           ),
                         ))
             ],
           );
 
+    // Return the final widget tree
     return !isSizeDetermined
         ? Positioned(left: position.dx, top: position.dy, child: dialogContents)
         : Stack(
